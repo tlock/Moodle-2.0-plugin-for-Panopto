bb922114625c85a498e18c6741310358764e7158
Peter Lancaster <peterlancast@gmail.com>
2015-11-11 16:52:03 -0800

    Revert "Only register observers for rolling sync if version is high enough"
    This reverts commit 06a6e5de2a8715107b36e89b4d0ff03c0cc1e1e4.

--- db/events.php
+++ db/events.php
@@ -21,38 +21,26 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-require_once(dirname(__FILE__) . '/../../../config.php');
-global $CFG;
 
-//Moodle version 2.7 or higher required for rolling sync tasks.
-$requiredVersion = 2014051200; 
-
-if($CFG->version > $requiredVersion)
-{
-    $observers = array(
-        // User enrolled event.
-        array(
-            'eventname' => '\core\event\user_enrolment_created',
-            'callback' => 'block_panopto_rollingsync::enrolmentcreated',
-        ),
-        // User unenrolled event.
-        array(
-            'eventname' => '\core\event\user_enrolment_deleted',
-            'callback' => 'block_panopto_rollingsync::enrolmentdeleted',
-        ),
-        // Event when user has role added to enrollment.
-        array(
-            'eventname' => '\core\event\role_assigned',
-            'callback' => 'block_panopto_rollingsync::roleadded',
-        ),
-        // Event when user has role removed from enrollment.
-        array(
-            'eventname' => '\core\event\role_unassigned',
-            'callback' => 'block_panopto_rollingsync::roledeleted',
-        ),
-    );
-}
-else
-{
-    $observers = array();
-}
+$observers = array(
+    // User enrolled event.
+    array(
+        'eventname' => '\core\event\user_enrolment_created',
+        'callback' => 'block_panopto_rollingsync::enrolmentcreated',
+    ),
+    // User unenrolled event.
+    array(
+        'eventname' => '\core\event\user_enrolment_deleted',
+        'callback' => 'block_panopto_rollingsync::enrolmentdeleted',
+    ),
+    // Event when user has role added to enrollment.
+    array(
+        'eventname' => '\core\event\role_assigned',
+        'callback' => 'block_panopto_rollingsync::roleadded',
+    ),
+    // Event when user has role removed from enrollment.
+    array(
+        'eventname' => '\core\event\role_unassigned',
+        'callback' => 'block_panopto_rollingsync::roledeleted',
+    ),
+);
