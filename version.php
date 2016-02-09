25da67cae84950d2d23e59f037e56f25eababe40
Peter Lancaster <peterlancast@gmail.com>
2016-02-09 10:58:10 -0800

    Update version.php

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 //Current internal version number.
-$internalversion = 2016011901;
+$internalversion = 2016020901;
 
 //Initialize $plugin object if it hasn't been already
 $plugin = (isset($plugin) ? $plugin : new stdClass());
