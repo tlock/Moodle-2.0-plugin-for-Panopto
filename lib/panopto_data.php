9d9cc577729d04c4178373049131dba287bce390
jmalmsten-panopto <jmalmsten@panopto.com>
2016-09-29 13:33:25 -0700

    [Github - 34] prevent exploding of empty strings

--- lib/panopto_data.php
+++ lib/panopto_data.php
@@ -439,21 +439,23 @@ class panopto_data {
     public static function get_course_role_mappings($moodlecourseid) {
         global $DB;
 
-        // Get publisher roles as string and explode to array.
-        $pubrolesraw = $DB->get_field(
-            'block_panopto_foldermap',
-            'publisher_mapping',
-            array('moodleid' => $moodlecourseid)
-        );
-        $pubroles = explode(',', $pubrolesraw);
+        $pubroles = array();
+        $creatorroles = array();
 
-        // Get creator roles as string, then explode to array.
-        $createrolesraw = $DB->get_field(
+         // Get publisher roles as string and explode to array
+        $rolesraw = $DB->get_record(
             'block_panopto_foldermap',
-            'creator_mapping',
-            array('moodleid' => $moodlecourseid)
+            array('moodleid' => $moodle_course_id),
+            'publisher_mapping, creator_mapping'
         );
-        $creatorroles = explode(',', $createrolesraw);
+
+        if ($rolesraw && !empty($rolesraw->publisher_mapping)) {
+            $pubroles = explode("," , $rolesraw->publisher_mapping);
+        }
+
+        if ($rolesraw && !empty($rolesraw->creator_mapping)) {
+            $creatorroles = explode(",", $rolesraw->creator_mapping);
+        }
 
         return array('publisher' => $pubroles, 'creator' => $creatorroles);
     }
