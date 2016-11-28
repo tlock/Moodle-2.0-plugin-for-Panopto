a6d4f933b68bbed6d6b67d4accf6b541cc21a0e2
jmalmsten-panopto <jmalmsten@panopto.com>
2016-11-28 11:54:25 -0800

    [TFS-43827] no longer require version.php in settings (#41)

--- settings.php
+++ settings.php
@@ -22,7 +22,6 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 defined('MOODLE_INTERNAL') || die;
-require('version.php');
 require_once(dirname(__FILE__) . '/classes/admin/trim_configtext.php');
 global $CFG;
 
